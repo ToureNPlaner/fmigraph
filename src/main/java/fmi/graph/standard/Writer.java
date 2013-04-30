3f5f9559729648385e12765dd25f943d26159870
Niklas Schnelle <niklas@komani.de>
2013-04-30 19:48:18 +0200

    Add license file and headers

--- src/main/java/fmi/graph/standard/Writer.java
+++ src/main/java/fmi/graph/standard/Writer.java
@@ -1,15 +1,22 @@
+/*
+ * Copyright 2013 FMI Universität Stuttgart
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package fmi.graph.standard;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import fmi.graph.definition.Node;
 import fmi.graph.definition.Edge;
+import fmi.graph.definition.Node;
 import fmi.graph.exceptions.InvalidFunctionException;
 import fmi.graph.metaio.MetaData;
 import fmi.graph.metaio.MetaWriter;
